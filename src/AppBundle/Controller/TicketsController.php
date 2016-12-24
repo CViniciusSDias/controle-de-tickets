482c0ce74877a9cf97b5b760f8277471392f2b48
Vinicius Dias <carlosv775@gmail.com>
2016-12-24 15:49:18 -0200

    Refatoração da criação do form e adição de comentários no código.

--- src/AppBundle/Controller/TicketsController.php
+++ src/AppBundle/Controller/TicketsController.php
@@ -7,12 +7,15 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\{SubmitType, TextareaType, TextType};
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\{Request, Response};
 
 class TicketsController extends Controller
 {
     /**
+     * Raiz do projeto
      * @Route("/")
+     * @return Response
      */
     public function indexAction(): Response
     {
@@ -21,26 +24,22 @@ class TicketsController extends Controller
 
     /**
      * @Route("/tickets/novo", name="cadastrar_ticket")
+     * @param Request $request
+     * @return Response
      */
     public function cadastrarAction(Request $request): Response
     {
         $ticket = new Ticket();
-        $form = $this->createFormBuilder($ticket)
-            ->add('titulo', TextType::class)
-            ->add('descricao', TextareaType::class, ['required' => false])
-            ->add('categoria', EntityType::class, [
-                'class' => 'AppBundle:Categoria',
-                'choice_label' => 'nome',
-                'placeholder' => 'Selecione'
-            ])->add('salvar', SubmitType::class, ['label' => 'Salvar'])
-            ->getForm();
+        $form = $this->criarForm($ticket);
         $form->handleRequest($request);
 
+        /** Caso seja uma requisição post, e o formulário já tenha sido enviado */
         if ($form->isSubmitted()) {
             $ticket = $form->getData();
             $validador = $this->get('validator');
             $erros = $validador->validate($ticket);
 
+            /** Se o ticket passar na validação, salva no BD e recarrega a página */
             if (count($erros) === 0) {
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($ticket);
@@ -59,5 +58,23 @@ class TicketsController extends Controller
         return $this->render('tickets/cadastrar.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * Factory Method do formulário para inserção de ticket
+     * @param Ticket $ticket
+     * @return FormInterface
+     */
+    private function criarForm(Ticket $ticket): FormInterface
+    {
+        return $this->createFormBuilder($ticket)
+            ->add('titulo', TextType::class)
+            ->add('descricao', TextareaType::class, ['required' => false])
+            ->add('categoria', EntityType::class, [
+                'class' => 'AppBundle:Categoria',
+                'choice_label' => 'nome',
+                'placeholder' => 'Selecione'
+            ])->add('salvar', SubmitType::class, ['label' => 'Salvar'])
+            ->getForm();
     }
 }
