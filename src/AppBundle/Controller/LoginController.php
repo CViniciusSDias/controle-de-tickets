c3e4818c44a9c86e7c80916a349a478f601a0fbd
Vinicius Dias <carlosv775@gmail.com>
2016-12-30 23:36:36 -0200

    Formulário de login e função de logout implementados.

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -8,8 +8,20 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController
+
+class LoginController extends Controller
 {
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        $auth = $this->get('security.authentication_utils');
+        $erro = $auth->getLastAuthenticationError();
 
+        return $this->render('login.html.twig', compact('erro'));
+    }
 }
