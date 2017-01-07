80336a807edcb5d1e48af04d2cbf947046bc770c
Vinicius Dias <carlosv775@gmail.com>
2017-01-07 10:04:42 -0200

    ModelTrait editada para utilizar apenas os métodos getters e setters, mas nunca acessar as propriedades diretamente.

--- src/AppBundle/Entity/ModelTrait.php
+++ src/AppBundle/Entity/ModelTrait.php
@@ -7,17 +7,11 @@ trait ModelTrait
 {
     public function __set($propriedade, $valor)
     {
-        if ($propriedade === 'id')
-            throw new DomainException('ID é de somente leitura');
-
         $metodo = 'set' . ucfirst($propriedade);
         if (method_exists($this, $metodo))
             return $this->$metodo($valor);
 
-        if (!property_exists($this, $propriedade))
-            throw new DomainException('Propriedade inválida');
-
-        $this->$propriedade = $valor;
+        throw new DomainException('Propriedade inválida');
     }
 
     public function __get($propriedade)
@@ -26,15 +20,6 @@ trait ModelTrait
         if (method_exists($this, $metodo))
             return $this->$metodo();
 
-        if (property_exists($this, $propriedade))
-            return $this->$propriedade;
-
         throw new DomainException('Propriedade inválida');
-    }
-
-    public function __call(string $metodo, array $args)
-    {
-        $propriedade = lcfirst(str_replace('get', '', $metodo));
-        return $this->$propriedade;
     }
 }
