7a688123d40e300917c326b675e69163f79b4701
Vinicius Dias <carlosv775@gmail.com>
2017-01-08 20:10:31 -0200

    Implementada a funcionalidade de cadastro de um novo usuário

--- src/AppBundle/Controller/UsuariosController.php
+++ src/AppBundle/Controller/UsuariosController.php
@@ -28,25 +28,40 @@ class UsuariosController extends Controller
      */
     public function cadastrarAction(Request $request): Response
     {
-        $usuario = new Usuario();
-        $form = $this->createFormBuilder($usuario)
-            ->add('nome', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('senha', PasswordType::class)
-            ->add('salvar', SubmitType::class)
-            ->add('tipo', ChoiceType::class, [
-                'choices' => [
-                    'Tipo' => '',
-                    'Usuário' => 'ROLE_USER',
-                    'Suporte' => 'ROLE_ADMIN',
-                    'Administrador' => 'ROLE_SUPER_ADMIN'
-                ]
-            ])
-            ->getForm();
-        $form->handleRequest($request);
+        try {
+            $usuario = new Usuario();
+            $form = $this->createFormBuilder($usuario)
+                ->add('nome', TextType::class)->add('email', EmailType::class)
+                ->add('senha', PasswordType::class)->add('salvar', SubmitType::class)
+                ->add('tipo', ChoiceType::class, [
+                    'choices' => [
+                        'Tipo' => '',
+                        'Usuário' => 'ROLE_USER',
+                        'Suporte' => 'ROLE_ADMIN',
+                        'Administrador' => 'ROLE_SUPER_ADMIN'
+                    ]
+                ])
+                ->getForm();
+            $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            echo 'Teste';
+            if ($form->isSubmitted()) {
+                $usuario = $form->getData();
+                $encoder = $this->container->get('security.password_encoder');
+                $usuario->setSenha($encoder->encodePassword($usuario, $usuario->getSenha()));
+                $validador = $this->get('validator');
+                $erros = $validador->validate($usuario);
+                if (count($erros) > 0) {
+                    throw new \InvalidArgumentException($erros[0]->getMessage());
+                }
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($usuario);
+                $em->flush();
+                $this->addFlash('success', "Usuário {$usuario->getNome()} adicionado com sucesso");
+
+                return $this->redirect($request->getUri());
+            }
+        } catch (\InvalidArgumentException $e) {
+            $this->addFlash('danger', $e->getMessage());
         }
 
         return $this->render('usuarios/cadastrar.html.twig', ['form' => $form->createView()]);
