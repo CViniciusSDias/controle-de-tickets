45d7ebb65f49423fd68f3c81d3dd7ddb952fc216
Vinicius Dias <carlosv775@gmail.com>
2017-01-27 00:33:21 -0200

    Gerador de documentação 'SAMI' adicionado.

---
+++ sami.php
@@ -0,0 +1,15 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->in('src')
+;
+
+return new Sami($iterator, ['build_dir' => __DIR__ . '/docs/sami']);
