bdd97f56fb96013648d553546478249ff18e71c4
CViniciusSDias <carlosv775@gmail.com>
2017-02-21 11:59:44 -0300

    Annotation de rota importada.

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class IndexController extends Controller
 {
